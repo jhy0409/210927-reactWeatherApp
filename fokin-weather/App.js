bebeebfabebd3cb94e4ee5604eb51e65603d0f08
jhy0409 <inooph@naver.com>
2021-09-27 16:22:41 +0900

    chap 1.1

--- fokin-weather/App.js
+++ fokin-weather/App.js
@@ -1,30 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Loading from './Loading';
+// import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      {/* <StatusBar style="auto" /> */}
-      <View style={styles.yellowView}></View>
-      <View style={styles.blueView}></View>
-    </View>
-  );
+  return <Loading />;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    // alignItems: 'center',
-    justifyContent: 'center'
-  }, 
-  yellowView: {
-    flex: 1,
-    backgroundColor: 'yellow'
-  }, 
-  blueView: {
-    flex: 1, 
-    backgroundColor: 'blue'
-  }
-});
