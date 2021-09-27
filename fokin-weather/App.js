afcb22818f61a3e5645e3894538d3e25d92ec5cf
jhy0409 <inooph@naver.com>
2021-09-27 18:28:00 +0900

    chap 2.0

--- fokin-weather/App.js
+++ fokin-weather/App.js
@@ -4,6 +4,7 @@ import { Alert } from 'react-native';
 import Loading from './Loading';
 import * as Location from 'expo-location';
 import axios from 'axios';
+import Weather from './Weather';
 
 const API_KEY = 'a76cd9cfef2f10a1c49aadb8ac7e1390';
 // https://api.openweathermap.org/data/2.5/weather?lat=37.785834&lon=-122.406417&appid=a76cd9cfef2f10a1c49aadb8ac7e1390
@@ -12,8 +13,9 @@ export default class extends React.Component {
     isLoading: true
   };
   getWeather = async(latitude, longitude) => {
-      const { data } = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+      const { data } = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
       );
+      this.setState({isLoading:false, temp: data.main.temp})
       console.log(data);
   }
   getLocation = async () => {
@@ -28,7 +30,7 @@ export default class extends React.Component {
       // latitude": 37.785834
       // "longitude": -122.406417
       this.getWeather(latitude, longitude);
-      this.setState({isLoading: true});
+      this.setState({isLoading: false});
       console.log(coords);
     } catch (error) {
       Alert.alert("Can't find you.","So sad");
@@ -40,8 +42,8 @@ export default class extends React.Component {
     this.getLocation();
   }
   render() {
-    const { isLoading } = this.state
-    return isLoading ? <Loading /> : null;
+    const { isLoading, temp } = this.state
+    return isLoading ? <Loading /> : <Weather temp={Math.round(temp)}/>;
   }
 }
 
