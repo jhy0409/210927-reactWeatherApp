e3f11d7c95767a6a8d83783b0225b3df76260bb9
jhy0409 <inooph@naver.com>
2021-09-28 11:04:44 +0900

    chap 2.2

--- fokin-weather/Weather.js
+++ fokin-weather/Weather.js
@@ -1,17 +1,25 @@
 import React from "react";
-import {View, Text, StyleSheet} from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import PropTypes from "prop-types";
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
-export default function Weather({temp}) {
+export default function Weather({ temp }) {
     return (
-    <View style={styles.container}>
-        <Text>{temp}</Text>
+        <View style={styles.container}>
+            <View style={styles.halfContainer}>
+                <MaterialCommunityIcons name="weather-lightning-rainy" size={96} color="black" />
+                <Text style={styles.temp}>{temp}º</Text>
+            </View>
+
+            <View style={styles.halfContainer}>
+
+            </View>
         </View>
-        );
+    );
 }
 
 Weather.propTypes = {
-    temp:PropTypes.number.isRequired,
+    temp: PropTypes.number.isRequired,
     condition: PropTypes.oneOf([
         "Thunderstorm",
         "Drizzle",
@@ -23,13 +31,21 @@ Weather.propTypes = {
         "Haze",
         "Mist",
         "Dust"
-      ]).isRequired
+    ]).isRequired
 };
 
 const styles = StyleSheet.create({
-   container: {
-       flex: 1, 
-       justifyContent: 'center',
-       alignItems: 'center'
-   } 
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    temp: {
+        fontSize: 42
+    },
+    halfContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
 });
