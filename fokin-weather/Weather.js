010618e86b9ad0fabb0871f7b0e2058279f0b22e
jhy0409 <inooph@naver.com>
2021-09-28 12:56:19 +0900

    chap 2.4

--- fokin-weather/Weather.js
+++ fokin-weather/Weather.js
@@ -5,46 +5,66 @@ import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { LinearGradient } from 'expo-linear-gradient';
 
 const weatherOptions = {
-    Haze: {
-        iconName: "weather-hail",
-        gradient: ["#4DA0B0", "#D39D38"]
-    },
     Thunderstorm: {
-        iconName: "",
-        gradient: ["", ""]
-    },
-    Drizzle: {
-        iconName: "", 
-        gradient: ["", ""]
-    },
-    Rain: {
-        iconName: "",
-        gradient: ["", ""]
-    },
-    Snow: {
-        iconName: "",
-        gradient: ["", ""]
-    },
-    Atmosphere: {
-        iconName: "",
-        gradient: ["", ""]
-    },
-    Clear: {
-        iconName: "",
-        gradient: ["", ""]
-    },
-    Clouds: {
-        iconName: "",
-        gradient: ["", ""]
-    },
-    Mist: {
-        iconName: "",
-        gradient: ["", ""]
-    },
-    Dust: {
-        iconName: "",
-        gradient: ["", ""]
-    }
+        iconName: "weather-lightning",
+        gradient: ["#373B44", "#4286f4"],
+        title: "Thunderstorm in the house",
+        subtitle: "Actually, outside of the house"
+      },
+      Drizzle: {
+        iconName: "weather-hail",
+        gradient: ["#89F7FE", "#66A6FF"],
+        title: "Drizzle",
+        subtitle: "Is like rain, but gay 🏳️‍🌈"
+      },
+      Rain: {
+        iconName: "weather-rainy",
+        gradient: ["#00C6FB", "#005BEA"],
+        title: "Raining like a MF",
+        subtitle: "For more info look outside"
+      },
+      Snow: {
+        iconName: "weather-snowy",
+        gradient: ["#7DE2FC", "#B9B6E5"],
+        title: "Cold as balls",
+        subtitle: "Do you want to build a snowman?"
+      },
+      Atmosphere: {
+        iconName: "weather-hail",
+        gradient: ["#89F7FE", "#66A6FF"],
+        title: "Atmosphere",
+        subtitle: "subtitle Atmosphere - is simply dummy text of the printing and typesetting industry. "
+      },
+      Clear: {
+        iconName: "weather-sunny",
+        gradient: ["#FF7300", "#FEF253"],
+        title: "Sunny",
+        subtitle: "subtitle Sunny - is simply dummy text of the printing and typesetting industry. "
+      },
+      Clouds: {
+        iconName: "weather-cloudy",
+        gradient: ["#D7D2CC", "#304352"],
+        title: "Clouds",
+        subtitle: "subtitle Clouds - is simply dummy text of the printing and typesetting industry. "
+      },
+      Mist: {
+        iconName: "weather-hail",
+        gradient: ["#4DA0B0", "#D39D38"],
+        title: "Mist!",
+        subtitle: "It's like you have no glasses on."
+      },
+      Dust: {
+        iconName: "weather-hail",
+        gradient: ["#4DA0B0", "#D39D38"],
+        title: "Dusty",
+        subtitle: "Thanks a lot China"
+      },
+      Haze: {
+        iconName: "weather-hail",
+        gradient: ["#4DA0B0", "#D39D38"],
+        title: "Haze",
+        subtitle: "Just don't go outside."
+      }
 };
 
 export default function Weather({ temp, condition }) {
@@ -63,8 +83,9 @@ export default function Weather({ temp, condition }) {
                     <Text style={styles.temp}>{temp}º</Text>
                 </View>
 
-                <View style={styles.halfContainer}>
-
+                <View style={{ ...styles.halfContainer, ...styles.textContainer}}>
+                    <Text style={styles.title}>{weatherOptions[condition].title}</Text>
+                    <Text style={styles.subtitle}>{weatherOptions[condition].subtitle}</Text>
                 </View>
             </LinearGradient>
     );
@@ -100,5 +121,20 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    title:{
+        color: 'white',
+        fontSize: 44,
+        fontWeight: '300',
+        marginBottom: 10
+    },
+    subtitle: {
+        fontWeight: '600',
+        color: 'white',
+        fontSize: 24
+    },
+    textContainer: {
+        paddingHorizontal: 20,
+        alignItems: 'flex-start'
     }
 });
